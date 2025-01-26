8b1879a8660ab0df5f5a9a71dd555d2ac25c6132
Nikita <nikborisov545@gmail.com>
2025-01-26 20:07:21 +0300

    Практическое задание 1. Часть 1

--- 122Borisov/MainWindow.xaml.cs
+++ 122Borisov/MainWindow.xaml.cs
@@ -1,28 +1,104 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace _122Borisov
+
+namespace PracticalWork1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            shRadioButton.IsChecked = true;
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!ValidateInput())
+            {
+                return;
+            }
+            double x = double.Parse(xTextBox.Text);
+            double y = double.Parse(yTextBox.Text);
+            double result = CalculateFunction(x, y);
+            resultTextBox.Text = result.ToString();
+
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            xTextBox.Clear();
+            yTextBox.Clear();
+            resultTextBox.Clear();
+        }
+
+        private bool ValidateInput()
+        {
+            double x, y;
+
+            if (string.IsNullOrEmpty(xTextBox.Text) || string.IsNullOrEmpty(yTextBox.Text))
+            {
+                MessageBox.Show("Заполните все поля ввода!", "Ошибка ввода", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if (!double.TryParse(xTextBox.Text, out x))
+            {
+                MessageBox.Show("Некорректный формат числа в поле x!", "Ошибка ввода", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (!double.TryParse(yTextBox.Text, out y))
+            {
+                MessageBox.Show("Некорректный формат числа в поле y!", "Ошибка ввода", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private double CalculateFunction(double x, double y)
+        {
+            double fx = 0;
+
+            if (shRadioButton.IsChecked == true)
+            {
+                fx = Math.Sinh(x);
+            }
+            else if (x2RadioButton.IsChecked == true)
+            {
+                fx = Math.Pow(x, 2);
+            }
+            else if (exRadioButton.IsChecked == true)
+            {
+                fx = Math.Exp(x);
+            }
+
+            double result;
+            if (x - y == 0)
+            {
+                result = Math.Pow(fx, 2) + Math.Pow(y, 2) + Math.Sin(y);
+            }
+            else if (x - y > 0)
+            {
+                result = Math.Pow((fx - y), 2) + Math.Cos(y);
+            }
+            else
+            {
+                result = Math.Pow((y - fx), 2) + Math.Tan(y);
+            }
+            return result;
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Вы действительно хотите выйти?", "Подтверждение выхода", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+
+            base.OnClosing(e);
         }
     }
 }
